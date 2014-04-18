afe75ccfc8ed3536e9d7144195c62555a916c6a8
Peter Shaw <shawty_ds@yahoo.com>
2014-04-18 15:36:12 +0100

    Fleshed out and implemented base MVC structure

---
+++ MvcExampleApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcExampleApp.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult Links()
+    {
+      return View();
+    }
+
+  }
+}
