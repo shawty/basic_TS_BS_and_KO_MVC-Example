4ead640d389659acfe85a384872ef4de2c85eefd
Peter Shaw <shawty_ds@yahoo.com>
2014-04-18 20:10:07 +0100

    Started adding typescript view model for main page

---
+++ MvcExampleApp/Scripts/personTable.js
@@ -0,0 +1,22 @@
+﻿var ViewModel = (function () {
+    function ViewModel(targetElement) {
+        this.showEmptyAlert = ko.observable(false);
+        this.showTable = ko.observable(false);
+        this.peopleData = ko.observableArray([]);
+        ko.applyBindings(this, targetElement);
+        $.support.cors = true;
+    }
+    ViewModel.prototype.Load = function () {
+        $.getJSON("spanked", function (data) {
+            alert(data);
+        });
+    };
+    return ViewModel;
+})();
+
+window.onload = function () {
+    var pageView = document.getElementById('peopleList');
+    var myViewModel = new ViewModel(pageView);
+    //myViewModel.Load();
+};
+//# sourceMappingURL=personTable.js.map
---
+++ MvcExampleApp/Scripts/personTable.ts
@@ -0,0 +1,28 @@
+﻿declare var ko: KnockoutStatic;
+declare var $: JQueryStatic;
+
+class ViewModel {
+  constructor(targetElement: HTMLElement) {
+    ko.applyBindings(this, targetElement);
+    $.support.cors = true;
+  }
+
+  public showEmptyAlert: any = ko.observable(false);
+  public showTable: any = ko.observable(false);
+  public peopleData: any = ko.observableArray([]);
+
+  public Load(): void {
+    $.getJSON("spanked",
+      (data) => {
+        alert(data);
+      }
+      );
+  }
+
+}
+
+window.onload = () => {
+  var pageView = document.getElementById('peopleList');
+  var myViewModel = new ViewModel(pageView);
+  //myViewModel.Load();
+};
