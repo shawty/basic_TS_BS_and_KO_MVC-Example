9a947f59b97e5f53b66536e63a539b40b5aada00
Peter Shaw <shawty_ds@yahoo.com>
2014-04-18 12:16:08 +0100

    Added class library for fake data source.

---
+++ PretendData/DataSource.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PretendData.Entities;
+
+namespace PretendData
+{
+  public static class DataSource
+  {
+    private static readonly List<PersonObject> _theData = new List<PersonObject>
+    {
+      new PersonObject { RecordId = 1, FirstName = "Peter", SecondName = "Shaw", EmailAddress = "peter.shaw@example.com" },
+      new PersonObject { RecordId = 2, FirstName = "Fred", SecondName = "Blogs", EmailAddress = "fred.bloggs@example.com" },
+      new PersonObject { RecordId = 3, FirstName = "Jane", SecondName = "Doe", EmailAddress = "jane.doe@example.com" },
+      new PersonObject { RecordId = 4, FirstName = "Alan", SecondName = "Person", EmailAddress = "a.person@example.com" }
+    };
+
+    public static List<PersonObject> GetAll()
+    {
+      return _theData;
+
+    }
+
+    public static PersonObject GetById(int id)
+    {
+      return _theData.FirstOrDefault(x => x.RecordId == id);
+
+    }
+
+    public static void DeleteById(int id)
+    {
+      _theData.RemoveAll(x => x.RecordId == id);
+
+    }
+
+    public static void Update(PersonObject personDetails)
+    {
+      var thePerson = _theData.SingleOrDefault(x => x.RecordId == personDetails.RecordId);
+      if(null != thePerson)
+      {
+        thePerson.FirstName = personDetails.FirstName;
+        thePerson.SecondName = personDetails.SecondName;
+        thePerson.EmailAddress = personDetails.EmailAddress;
+      }
+
+    }
+
+    public static void AddNew(PersonObject personDetails)
+    {
+      int newId = _theData.Max(x => x.RecordId) + 1;
+      personDetails.RecordId = newId;
+      _theData.Add(personDetails);
+
+    }
+
+  }
+}
---
+++ PretendData/Entities/PersonObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace PretendData.Entities
+{
+  public class PersonObject
+  {
+    public int RecordId { get; set; }
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+    public string EmailAddress { get; set; }
+  }
+}
